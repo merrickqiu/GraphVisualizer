65d4bf956dbc4bbdf171e4ee7910858daecc93ee
Merric <merrickqiu@gmail.com>
2022-03-15 14:30:13 -0700

    Code refactoring

--- src/main/java/com/example/graphvisualizer/GUI.java
+++ src/main/java/com/example/graphvisualizer/GUI.java
@@ -28,86 +28,92 @@ public class GUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Graph<String> graph = new Graph<>();
-        Display display = new Display(graph);
+        PropertyDisplay display = new PropertyDisplay(graph);
 
         Pane graphPane = new Pane();
         graphPane.setPrefSize(1600, 1200);
-        EventHandler<MouseEvent> vertexClick = event -> {
-            // Edge Creation/Deletion
-            if (event.isShiftDown()) {
-                // Select Start Node
-                if (startVertex == null) {
-                    startVertex = (Vertex)((Node)event.getTarget()).getParent();
-                    startVertex.setColor(Color.RED);
-                    System.out.println("Added Start Node");
+        EventHandler<MouseEvent> normalClick = event -> {
+            // Vertex Creation
+            if (event.getButton() == MouseButton.PRIMARY) {
+                graph.addVertex(vertexCount.toString());
+                display.refresh();
+
+                Vertex vertex = new Vertex(event.getSceneX(), event.getSceneY(), vertexCount.toString());
+                graphPane.getChildren().add(vertex);
+                vertexCount++;
+                System.out.printf("Added circle at x:%f y:%f\n", event.getSceneX(), event.getSceneY());
+            }
+            //Vertex Deletion
+            else if (event.getButton() == MouseButton.SECONDARY) {
+                Vertex vertex = (Vertex)((Node)event.getTarget()).getParent();
+                graph.removeVertex(vertex.getLabel());
+                display.refresh();
+
+                //Edge removal
+                while(!vertex.edges.isEmpty()) {
+                    Edge edge = vertex.edges.get(0);
+                    graphPane.getChildren().remove(edge);
+                    edge.delete();
                 }
-                // Select End Node
-                else {
-                    Vertex endVertex = (Vertex)((Node)event.getTarget()).getParent();
-                    // Don't allow self connection
-                    if (startVertex == endVertex) {
-                        System.out.println("Blocked self connection");
+                graphPane.getChildren().remove(vertex);
+                System.out.println("Removed Circle: " + event.getTarget());
+            }
+        };
+        EventHandler<MouseEvent> shiftClick = event -> {
+            // Select Start Node
+            if (startVertex == null) {
+                startVertex = (Vertex)((Node)event.getTarget()).getParent();
+                startVertex.setColor(Color.RED);
+                System.out.println("Added Start Node");
+            }
+            // Select End Node
+            else {
+                Vertex endVertex = (Vertex)((Node)event.getTarget()).getParent();
+                // Don't allow self connection
+                if (startVertex == endVertex) {
+                    System.out.println("Blocked self connection");
+                    startVertex.setColor(Color.WHITE);
+                    startVertex = null;
+                    return;
+                }
+                //Delete edge
+                for (Edge edge : startVertex.edges) { // jank way to test if edge already exists
+                    if (edge.v1 == startVertex && edge.v2 == endVertex ||
+                            edge.v1 == endVertex && edge.v2 == startVertex) {
+                        System.out.println("Deleting edge");
+                        graph.removeEdge(startVertex.getLabel(), endVertex.getLabel());
+                        display.refresh();
+
+                        graphPane.getChildren().remove(edge);
+                        startVertex.edges.remove(edge);
+                        endVertex.edges.remove(edge);
                         startVertex.setColor(Color.WHITE);
                         startVertex = null;
                         return;
                     }
-                    //Delete edge
-                    for (Edge edge : startVertex.edges) { // jank way to test if edge already exists
-                        if (edge.v1 == startVertex && edge.v2 == endVertex ||
-                            edge.v1 == endVertex && edge.v2 == startVertex) {
-                            System.out.println("Deleting edge");
-                            graph.removeEdge(startVertex.getLabel(), endVertex.getLabel());
-                            display.refresh();
-
-                            graphPane.getChildren().remove(edge);
-                            startVertex.edges.remove(edge);
-                            endVertex.edges.remove(edge);
-                            startVertex.setColor(Color.WHITE);
-                            startVertex = null;
-                            return;
-                        }
-                    }
-                    //Create edge
-                    graph.addEdge(startVertex.getLabel(), endVertex.getLabel());
-                    display.refresh();
-
-                    Edge edge = new Edge(startVertex, endVertex);
-                    graphPane.getChildren().add(edge);
-
-                    startVertex.addEdge(edge);
-                    endVertex.addEdge(edge);
-
-                    startVertex.setColor(Color.WHITE);
-                    startVertex = null;
-                    System.out.println("Created Edge");
                 }
+                //Create edge
+                graph.addEdge(startVertex.getLabel(), endVertex.getLabel());
+                display.refresh();
+
+                Edge edge = new Edge(startVertex, endVertex);
+                graphPane.getChildren().add(edge);
+
+                startVertex.addEdge(edge);
+                endVertex.addEdge(edge);
+
+                startVertex.setColor(Color.WHITE);
+                startVertex = null;
+                System.out.println("Created Edge");
+            }
+        };
+        EventHandler<MouseEvent> handleClick = event -> {
+            // Edge Creation/Deletion
+            if (event.isShiftDown()) {
+                shiftClick.handle(event);
             }
             else {
-                // Vertex Creation
-                if (event.getButton() == MouseButton.PRIMARY) {
-                    graph.addVertex(vertexCount.toString());
-                    display.refresh();
-
-                    Vertex vertex = new Vertex(event.getSceneX(), event.getSceneY(), vertexCount.toString());
-                    graphPane.getChildren().add(vertex);
-                    vertexCount++;
-                    System.out.printf("Added circle at x:%f y:%f\n", event.getSceneX(), event.getSceneY());
-                }
-                //Vertex Deletion
-                else if (event.getButton() == MouseButton.SECONDARY) {
-                    Vertex vertex = (Vertex)((Node)event.getTarget()).getParent();
-                    graph.removeVertex(vertex.getLabel());
-                    display.refresh();
-
-                    //Edge removal
-                    while(!vertex.edges.isEmpty()) {
-                        Edge edge = vertex.edges.get(0);
-                        graphPane.getChildren().remove(edge);
-                        edge.delete();
-                    }
-                    graphPane.getChildren().remove(vertex);
-                    System.out.println("Removed Circle: " + event.getTarget());
-                }
+                normalClick.handle(event);
             }
         };
 
@@ -118,14 +124,14 @@ public class GUI extends Application {
             vertex.refreshEdges();
         };
 
-        MouseHandler vertexMouseHandler = new MouseHandler(vertexClick, vertexDrag);
+        MouseHandler vertexMouseHandler = new MouseHandler(handleClick, vertexDrag);
         graphPane.addEventHandler(MouseEvent.ANY, vertexMouseHandler);
 
         GridPane root = new GridPane();
 
         root.add(graphPane, 0, 0, 1, 1);
         root.add(display, 1, 0, 1, 1);
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root, 800, 600);
         stage.setScene(scene);
         stage.setTitle("Graph Visualizer");
         stage.show();
@@ -133,28 +139,5 @@ public class GUI extends Application {
 
     public static void main(String[] args) {
         launch();
-    }
-}
-
-class Display extends VBox {
-    private static final String minDegreeFormat = "Minimum Degree: %d";
-    private static final String maxDegreeFormat = "Maximum Degree: %d";
-    private static final String bipartiteFormat = "Bipartite?: %b";
-    final Text title, minDegree, maxDegree, bipartite;
-    Graph graph;
-    public Display(Graph graph) {
-        this.graph = graph;
-        this.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY)));
-        title = new Text("----Graph properties----");
-        minDegree = new Text(String.format(minDegreeFormat, graph.getMinDegree()));
-        maxDegree = new Text(String.format(maxDegreeFormat, graph.getMaxDegree()));
-        bipartite = new Text(String.format(bipartiteFormat, graph.isBipartite()));
-        this.getChildren().addAll(title, minDegree, maxDegree, bipartite);
-    }
-
-    public void refresh() {
-        minDegree.setText(String.format(minDegreeFormat, graph.getMinDegree()));
-        maxDegree.setText(String.format(maxDegreeFormat, graph.getMaxDegree()));
-        bipartite.setText(String.format(bipartiteFormat, graph.isBipartite()));
     }
 }
---
+++ src/main/java/com/example/graphvisualizer/PropertyDisplay.java
@@ -0,0 +1,54 @@
+package com.example.graphvisualizer;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class PropertyDisplay extends VBox {
+    final Graph<String> graph;
+    List<Property> properties;
+
+    public PropertyDisplay(Graph<String> graph) {
+        this.graph = graph;
+        this.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY)));
+        properties = new ArrayList<>();
+        addProperty("Minimum Degree: ", graph::getMinDegree);
+        addProperty("Maximum Degree: ", graph::getMaxDegree);
+        addProperty("Bipartite: ", graph::isBipartite);
+    }
+
+    public void addProperty(String label, Supplier<?> propertySupplier) {
+        Property property = new Property(label, () -> String.valueOf(propertySupplier.get()));
+        properties.add(property);
+        this.getChildren().add(property);
+    }
+
+    public void refresh() {
+        properties.forEach(Property::refresh);
+    }
+}
+
+class Property extends Text {
+    String label;
+    Supplier<String> propertySupplier;
+    public Property(String label, Supplier<String> propertySupplier){
+        this.label = label;
+        this.propertySupplier = propertySupplier;
+        refresh();
+    }
+
+    public void refresh() {
+        this.setText(label + propertySupplier.get());
+    }
+}
+
+
