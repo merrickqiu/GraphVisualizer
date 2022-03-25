7c7d61afa4bb5948aa31d90b12d7f9be5cac7acd
Merric <merrickqiu@gmail.com>
2022-03-25 14:05:10 -0700

    commit

---
+++ out/make.bat
@@ -0,0 +1,8 @@
+jlink ^
+--module-path "C:\Program Files\Java\jdk-17\jmods" ^
+--add-modules=javafx.base ^
+--add-modules=javafx.controls ^
+--add-modules=javafx.fxml ^
+--add-modules=javafx.graphics ^
+--add-modules=javafx.swing ^
+--output C:\Users\merri\Documents\GitHub\GraphVisualizer\out\JRE\
